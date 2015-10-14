321f292251048e0503ffb52a5c0161904a52d681
Diego <diegoreyesmo@gmail.com>
2015-10-14 15:53:59 -0300

    ROI con area minima

--- app.py
+++ app.py
@@ -5,14 +5,9 @@ import time
 from common import draw_keypoints
 
 #file1 = '/home/diego/Dropbox/01Memoria/Data Sets/VIPeR/cam_a/000_45.bmp'
-file1 = '/home/diego/Dropbox/01Memoria/Data Sets/otros/Walk1.mpg'
+#file1 = '/home/diego/Dropbox/01Memoria/Data Sets/otros/Walk1.mpg'
 #file1 = '/home/diego/Videos-REID/Videos-Inchalam/Estacionamiento/14300200.avi'
-
-'''
-img = cv2.imread(file1)
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-'''
-
+file1 = '/home/diego/Videos-REID/micc_surveillance_dataset/car1_0.avi'
 
 detector = cv2.xfeatures2d.SIFT_create()
 #norm = cv2.NORM_L2
@@ -23,46 +18,58 @@ detector = cv2.xfeatures2d.SIFT_create()
 #kp1, desc1 = detector.detectAndCompute(frame, None)
 #kp2, desc2 = detector.detectAndCompute(img2, None)
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(file1)
 
 fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
 i=0
-
+levels=1
 while(True):
-    t0 = time.clock()
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    fgmask = fgbg.apply(frame) # detectar primer plano en movimiento
-    retval, thresh = cv2.threshold(fgmask, 200, 256, cv2.THRESH_BINARY); #eliminar sombra
-    enmask = cv2.cvtColor(thresh, cv2.COLOR_GRAY2RGB); #triplicar canales, para poder compararlos con un frame en RGB
-    enmask = cv2.bitwise_and(frame,enmask)
-    '''
+	t0 = time.clock() # calcular tiempo
+	ret, frame = cap.read() # Capture frame-by-frame
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # convertir frame a escala de grises
+	fgmask = fgbg.apply(frame) # detectar primer plano en movimiento
+	retval, thresh = cv2.threshold(fgmask, 200, 256, cv2.THRESH_BINARY); #eliminar sombra
+	enmask = cv2.cvtColor(thresh, cv2.COLOR_GRAY2RGB); #triplicar canales, para poder compararlos con un frame en RGB
+	enmask = cv2.bitwise_and(frame,enmask) #enmascarar frame original
+	_, contours0,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+	contours = [cv2.approxPolyDP(cnt, 3, True) for cnt in contours0]
+	#cv2.drawContours( frame, contours, (-1, 3)[levels <= 0], (128,255,255),1, cv2.LINE_AA, hierarchy, abs(levels) )
+    #    cv2.imshow('contours', vis)
+	'''
     if i%24 == 0:
-        kp1, desc1 = detector.detectAndCompute(fgmask, None)
-        draw_keypoints(frame,kp1)
-    
-        #cv2.imshow('frame2',fgmask)
-    '''
-    # Display the resulting frame
-    #print 'frame ',i
-    
-        
-        #t1 = time.clock() - t0
-    
-        #print 1. / t1
-        #t0 = t1
-    cv2.imshow('frame',frame)
-    #cv2.imshow('fgmask',fgmask)
-    cv2.imshow('thresh',thresh)
-    cv2.imshow('enmask',enmask)
-    i=i+1
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    	kp1, desc1 = detector.detectAndCompute(fgmask, None)
+    	draw_keypoints(frame,kp1)
+    	#cv2.imshow('frame2',fgmask)
 
-        # When everything done, release the capture
-        cap.release()
-        cv2.destroyAllWindows()
+	'''
+	
+	# Find the index of the largest contour
+	areas = [cv2.contourArea(c) for c in contours]
+	if len(areas) > 0:
+		max_index = np.argmax(areas)
+		cnt=contours[max_index]
+		x,y,w,h = cv2.boundingRect(cnt)
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),1)
+		area = w * h
+		#print 'area: ', w*h
+		if area > 1000:
+			kp1, desc1 = detector.detectAndCompute(enmask, None)
+			draw_keypoints(frame,kp1)
+			#cv2.imshow('frame2',f)
+	
+    # Display the resulting frame
+	#cv2.imshow('fgmask',fgmask)
+	#cv2.imshow('thresh',thresh)
+	#cv2.imshow('enmask',enmask)
+	#cv2.imshow('gray',gray)
+	cv2.imshow('frame',frame)
+	t1 = time.clock() - t0
+	t0 = t1
+	#print 'frame:',i, ' rate: ', 1. / t1
+	i=i+1
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		# When everything done, release the capture
+		cap.release()
+		cv2.destroyAllWindows()
+		break
 
